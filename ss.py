9c55945862c2334b440541ea08ad329b82f97f7e
JAEHEE RYU <45359366+JaeheeRyu@users.noreply.github.com>
2021-05-25 01:16:41 +0900

    Delete ss.py

--- ss.py
+++
@@ -1,9 +0,0 @@
-from beautifultable import BeautifulTable
-table = BeautifulTable()
-#table.column_headers = ["Title", "rank", "gender"]
-table.append_row(["Jacob", 1, "boy"])
-table.append_row(["Isabella", 1, "girl"])
-table.append_row(["Ethan", 2, "boy"])
-table.append_row(["Sophia", 2, "girl"])
-table.append_row(["Michael", 3, "boy"])
-print(table)
